6e96565115185a18d20f330322674d0eadf640a3
Joshua <joshuapperry4@gmail.com>
2022-06-20 21:27:46 -0600

    Declared all views and controllers.
    Wanted to get all views declared for setting main menu event handlers.

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
         primaryStage.setTitle(("First Screen"));
-        primaryStage.setScene(new Scene(root, 900, 300));
+        primaryStage.setScene(new Scene(root, 900, 325));
         primaryStage.show();
     }
 
