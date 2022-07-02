51e1beb75757ed5753e53d40b6c451d86e880ad4
Joshua <joshuapperry4@gmail.com>
2022-07-02 10:12:16 -0600

    Main Menu FxIds
    Declared main menu Fx Ids in  FXML and in controller.

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -3,9 +3,76 @@ package controller;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class MainMenuController implements Initializable {
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
+
+    @FXML
+    private Button exitProgramButton;
+
+    @FXML
+    private Button partAddButton;
+
+    @FXML
+    private Button partModifyButton;
+
+    @FXML
+    private Button partDeleteButton;
+
+    @FXML
+    private TableView<?> partTableView;
+
+    @FXML
+    private TableColumn<?, ?> partIdCol;
+
+    @FXML
+    private TableColumn<?, ?> partNameCol;
+
+    @FXML
+    private TableColumn<?, ?> partStockCol;
+
+    @FXML
+    private TableColumn<?, ?> partPriceCol;
+
+    @FXML
+    private TextField partSearchBar;
+
+    @FXML
+    private Button productAddButton;
+
+    @FXML
+    private Button productModifyButton;
+
+    @FXML
+    private Button productDeleteButton;
+
+    @FXML
+    private TableView<?> productTableView;
+
+    @FXML
+    private TableColumn<?, ?> productIdCol;
+
+    @FXML
+    private TableColumn<?, ?> productNameCol;
+
+    @FXML
+    private TableColumn<?, ?> productStockCol;
+
+    @FXML
+    private TableColumn<?, ?> productPriceCol;
+
+    @FXML
+    private TextField productSearchBar;
+
+
+
+
 }
