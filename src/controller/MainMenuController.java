8bd58379de48291469ffef81695ef942b307ba30
Joshua <joshuapperry4@gmail.com>
2022-07-03 13:57:18 -0600

    refactored test data
    Moved addTestData to Inventory.java. Called it in a static initializer. Now will only call and populate once. Done to prevent duplicates.

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -90,16 +90,7 @@ public class MainMenuController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        InHouse bolt = new InHouse(1, "Bolt", 1.25, 10, 5, 15, 19);
-        Inventory.addPart(bolt);
-        Outsourced pipe = new Outsourced(2, "Pipe", 10.00, 3, 1, 5, "Pipes-R-Us");
-        Inventory.addPart(pipe);
-        InHouse bracket = new InHouse(3, "Bracket", 5.00, 7, 3, 11, 10);
-        Inventory.addPart((bracket));
-
         partTableView.setItems(Inventory.getAllParts());
-
         partIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
         partNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
         partStockCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
