89d1031e116ee80021295444d32645750ad00ec3
Joshua <joshuapperry4@gmail.com>
2022-07-11 16:32:01 -0600

    Future enhancement doc

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,16 @@ import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
 
-/** Main class for program.*/
+/** Main class for program.
+ * FUTURE ENHANCEMENT - Under the Navigation class there is a method called navWithData.
+ * I was trying to create a nav class that would move to either modify screen.
+ * This would cut down on some redundant code.
+ * I ran into some problems getting the selection via the Navigation class.
+ * I couldn't make the table views static and if I made a new MainMenuController within the Nav class I would crash via an infinite loop.
+ * Need look more into this.
+ * For a feature I would like to add communication to an online store.
+ * This would automatically adjust stock and therefore parts available.
+ * Once a part hits a min stock value a request to purchase more is prompted to the user.*/
 public class Main extends Application {
 
     /** Start life cycle method of JavaFX.
